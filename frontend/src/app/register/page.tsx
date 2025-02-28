57bb8f6bb473937da58d93cc070116d8f34cca47
Gabriel Macena <gabriel.macena10@hotmail.com>
2025-02-28 12:39:38 -0300

    Tela de cadastro

---
+++ frontend/src/app/register/page.tsx
@@ -0,0 +1,11 @@
+import RegisterForm from "../../components/RegisterForm";
+
+export default function Register() {
+  return (
+    <main className="flex min-h-screen items-center justify-center bg-gray-50 p-4">
+      <div className="w-full max-w-lg rounded-lg bg-white p-12 shadow-md">
+        <RegisterForm />
+      </div>
+    </main>
+  );
+}
