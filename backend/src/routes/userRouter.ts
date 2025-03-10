3f8118367dfad142e72d34c0d62bc8aa8e71ce73
Marcela <marcelamartinstec@gmail.com>
2025-03-10 18:49:11 -0300

    BOOK-34 ajustando rotas

--- backend/src/routes/userRouter.ts
+++ backend/src/routes/userRouter.ts
@@ -1,13 +1,12 @@
 import { Router } from "express";
 import userController from "../controllers/userController";
-import authMiddleware from "../middlewares/authMiddleware"; 
 
 const router = Router();
 
 router.post("/", userController.register);
 router.post("/login", userController.login);
-router.get("/:uuid", authMiddleware, userController.getProfile);
-router.put("/:uuid", authMiddleware, userController.updateProfile);
-router.delete("/:uuid", authMiddleware, userController.deleteProfile);
+router.get("/:uuid", userController.getProfile); 
+router.put("/:uuid", userController.updateProfile); 
+router.delete("/:uuid", userController.deleteProfile); 
 
 export default router;
