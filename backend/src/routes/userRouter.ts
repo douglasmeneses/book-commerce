18d7aef1157757b9b7bd2ee04b99c1738ddbd414
Marcela <marcelamartinstec@gmail.com>
2025-03-13 13:18:42 -0300

    BOOK-34 ajuste de rotas segundo typescript

--- backend/src/routes/userRouter.ts
+++ backend/src/routes/userRouter.ts
@@ -1,12 +1,22 @@
-import { Router } from "express";
+import { Router, Request, Response } from "express";
 import userController from "../controllers/userController";
 
 const router = Router();
 
-router.post("/", userController.register);
-router.post("/login", userController.login);
-router.get("/:uuid", userController.getProfile); 
-router.put("/:uuid", userController.updateProfile); 
-router.delete("/:uuid", userController.deleteProfile); 
+router.post("/", (req: Request, res: Response) => {
+  userController.register(req, res);
+});
+router.post("/login", (req: Request, res: Response) => {
+    userController.login(req, res);
+})
+router.get("/:uuid", (req: Request, res: Response) => {
+    userController.getProfile(req, res)
+})
+router.put("/:uuid", (req: Request, res: Response) =>{
+    userController.updateProfile(req, res)
+})
+router.get("/:uuid", (req: Request, res: Response) => {
+    userController.deleteProfile(req, res)
+})
 
 export default router;
