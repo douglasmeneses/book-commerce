848821ce2afb765c159364a4b5e58d602808acfa
Slow HD <jeffsonluiz11@gmail.com>
2025-03-19 23:44:37 -0300

    BOOK-75 adicionei a func auth Middleware às rotas

--- backend/src/routes/userRouter.ts
+++ backend/src/routes/userRouter.ts
@@ -1,26 +1,26 @@
-import { Router, Request, Response } from 'express';
-import * as userController from '../controllers/userController'; 
+import { Router, Request, Response } from "express";
+import * as userController from "../controllers/userController";
+import { authMiddleware } from "../middlewares/auth";
 
 const router = Router();
 
-router.post('/register', (req: Request, res: Response) => {
+router.post("/register", (req: Request, res: Response) => {
   userController.registerUser(req, res);
 });
 
-// Rota de login de usuário
-router.post('/login', (req: Request, res: Response) => {
+router.post("/login", (req: Request, res: Response) => {
   userController.loginUser(req, res);
 });
 
-router.get('/:uuid', (req: Request, res: Response) => {
+router.get("/:uuid", (req: Request, res: Response) => {
   userController.getUserProfile(req, res);
 });
 
-router.put('/:uuid', (req: Request, res: Response) => {
+router.put("/:uuid", authMiddleware, (req: Request, res: Response) => {
   userController.updateUserProfile(req, res);
 });
 
-router.delete('/:uuid', (req: Request, res: Response) => {
+router.delete("/:uuid", authMiddleware, (req: Request, res: Response) => {
   userController.deleteUser(req, res);
 });
 
