401d23147b7ccd2e91fa462ce65d04dcffd320fd
Slow HD <jeffsonluiz11@gmail.com>
2025-03-22 16:36:34 -0300

    BOOK-76 rotas de deletar cartItem e pegar cart criadas e pegando

--- backend/src/controllers/cartController.ts
+++ backend/src/controllers/cartController.ts
@@ -6,11 +6,67 @@ const cartController = {
     try {
       const user_uuid = req.params.user_uuid as string;
       const { book_uuid, quantity } = req.body;
-      const cart = await cartService.addBooktoCart(
+      const cart = await cartService.addBookToCart(
         user_uuid,
         book_uuid,
         quantity
       );
+      if ("error" in cart) {
+        return res.status(400).json({ error: cart.error });
+      }
+      return res.json(cart);
+    } catch (error) {
+      return res
+        .status(500)
+        .json({ error: error instanceof Error ? error.message : error });
+    }
+  },
+  removeBookToCart: async (req: Request, res: Response) => {
+    try {
+      const id = parseInt(req.params.id);
+      const user_uuid = req.body.user_uuid as string;
+      const cartItem_id = parseInt(req.body.cartItem_id);
+      const quantity = parseInt(req.body.quantity);
+
+      const cart = await cartService.removeBookToCart(
+        user_uuid,
+        id,
+        cartItem_id,
+        quantity
+      );
+      if ("error" in cart) {
+        return res.status(400).json({ error: cart.error });
+      }
+      return res.json(cart);
+    } catch (error) {
+      return res
+        .status(500)
+        .json({ error: error instanceof Error ? error.message : error });
+    }
+  },
+  getCartByUser_UUID: async (req: Request, res: Response) => {
+    try {
+      const user_uuid = req.params.user_uuid as string;
+      const cart = await cartService.getCartByUser_UUID(user_uuid);
+      if ("error" in cart) {
+        return res.status(400).json({ error: cart.error });
+      }
+      return res.json(cart);
+    } catch (error) {
+      return res
+        .status(500)
+        .json({ error: error instanceof Error ? error.message : error });
+    }
+  },
+  deleteCartItem: async (req: Request, res: Response) => {
+    try {
+      const cartItem_id = parseInt(req.params.cartItem_id);
+      const { user_uuid, id } = req.body as {
+        user_uuid: string;
+        id: number;
+      };
+
+      const cart = await cartService.deleteCartItem(user_uuid, id, cartItem_id);
       if ("error" in cart) {
         return res.status(400).json({ error: cart.error });
       }
--- backend/src/services/cartService.ts
+++ backend/src/services/cartService.ts
@@ -1,14 +1,17 @@
 import { Cart, PrismaClient } from "@prisma/client";
 import { userExists } from "../middlewares/userValidators";
 import { bookExists } from "../middlewares/bookValidators";
-import { cartValidates } from "../middlewares/cartValidators";
+import {
+  cartValidates,
+  removeBookToCartValidates,
+} from "../middlewares/cartValidators";
 import cartItemService from "./cartItem";
 import { error } from "../types/bookTypes";
 
 const prisma = new PrismaClient();
 
 const cartService = {
-  addBooktoCart: async (
+  addBookToCart: async (
     user_uuid: string,
     book_uuid: string,
     quantity: number
@@ -47,7 +50,7 @@ const cartService = {
         quantity
       );
       if ("error" in cartItem) {
-        return { error: cartItem.error };
+        return { error: cartItem.error as string };
       }
 
       await prisma.cart.update({
@@ -73,6 +76,143 @@ const cartService = {
         return { error: "Cart not found!" };
       }
       return cart;
+    } catch (error) {
+      return { error: error instanceof Error ? error.message : "error" };
+    }
+  },
+  getCartById: async (id: number): Promise<Cart | error> => {
+    try {
+      const cart = await prisma.cart.findFirst({
+        where: {
+          id: id,
+        },
+        include: {
+          CartItem: { include: { book: true } },
+        },
+      });
+
+      if (!cart) {
+        return { error: "Cart not found!" };
+      }
+
+      return cart;
+    } catch (error) {
+      return { error: error instanceof Error ? error.message : "error" };
+    }
+  },
+  getCartByUser_UUID: async (user_uuid: string) => {
+    try {
+      const user = await userExists(user_uuid);
+      if ("error" in user) {
+        return { error: user.error };
+      }
+
+      const cart = await prisma.cart.findFirst({
+        where: {
+          user_id: user.id,
+        },
+        include: {
+          CartItem: { include: { book: true } },
+        },
+      });
+
+      if (!cart) {
+        return { error: "Cart not found!" };
+      }
+
+      return cart;
+    } catch (error) {
+      return { error: error instanceof Error ? error.message : "error" };
+    }
+  },
+  removeBookToCart: async (
+    user_uuid: string,
+    id: number,
+    cartItem_id: number,
+    quantity: number
+  ): Promise<error | Cart> => {
+    try {
+      const validates = removeBookToCartValidates(id, cartItem_id, quantity);
+      if (validates) return { error: validates.error };
+
+      const user = await userExists(user_uuid);
+      if ("error" in user) return { error: user.error };
+
+      const cart = await cartService.getCartById(id);
+      if ("error" in cart) return { error: cart.error };
+
+      if (user.id !== cart.user_id)
+        return { error: "User not allowed to remove this item" };
+
+      const cartItem = await cartItemService.removeBookToCart(
+        cartItem_id,
+        quantity
+      );
+      if ("error" in cartItem) {
+        return { error: cartItem.error as string };
+      }
+
+      await prisma.cart.update({
+        where: {
+          id: id,
+        },
+        data: {
+          totalPrice:
+            cart.totalPrice?.toNumber() -
+            cartItem.book.price.toNumber() * quantity,
+        },
+      });
+
+      const updatedCart = await cartService.getCartById(id);
+      if ("error" in updatedCart) {
+        return { error: updatedCart.error };
+      }
+      return updatedCart;
+    } catch (error) {
+      return { error: error instanceof Error ? error.message : "error" };
+    }
+  },
+  deleteCartItem: async (
+    user_uuid: string,
+    id: number,
+    cartItem_id: number
+  ) => {
+    try {
+      const user = await userExists(user_uuid);
+      if ("error" in user) return { error: user.error };
+
+      const cart = await cartService.getCartById(id);
+      if ("error" in cart) return { error: cart.error };
+
+      if (user.id !== cart.user_id)
+        return { error: "User not allowed to remove this item" };
+
+      const cartItem = await cartItemService.getCartItemById(cartItem_id);
+      if ("error" in cartItem) return { error: cartItem.error };
+
+      if (cartItem.cart_id !== id)
+        return { error: "Cart item not found in this cart" };
+
+      await prisma.cart.update({
+        where: {
+          id: id,
+        },
+        data: {
+          totalPrice: cart.totalPrice?.toNumber() - cartItem.price.toNumber(),
+        },
+      });
+
+      const deleteCartItem = await cartItemService.deleteCartItem(cartItem_id);
+      if (deleteCartItem !== true && "error" in deleteCartItem)
+        return { error: deleteCartItem.error };
+
+      const updatedCart =
+        deleteCartItem === true
+          ? await cartService.getCartById(id)
+          : { error: "error" };
+      if ("error" in updatedCart) return { error: updatedCart.error };
+
+      return updatedCart;
     } catch (error) {
       return { error: error instanceof Error ? error.message : "error" };
     }
