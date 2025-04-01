bd391b876f3ba05623410d8d9a3de7503f666051
Slow HD <jeffsonluiz11@gmail.com>
2025-03-31 22:00:11 -0300

    add migrates

--- backend/prisma/migrations/20250327025140_adicionando_o_image_url_na_tabela_books/migration.sql
+++ backend/prisma/migrations/20250327025140_adicionando_o_image_url_na_tabela_books/migration.sql
@@ -4,5 +4,3 @@ ALTER TABLE "Author" ALTER COLUMN "image" DROP NOT NULL;
 -- AlterTable
 ALTER TABLE "Book" ADD COLUMN     "image_url" TEXT DEFAULT '';
 
--- AlterTable
-ALTER TABLE "OrderItem" ADD COLUMN     "price" DECIMAL(10,2) NOT NULL DEFAULT 0.00;
---
+++ backend/prisma/migrations/20250401005914_add_recomendacoes/migration.sql
@@ -0,0 +1,20 @@
+-- DropForeignKey
+ALTER TABLE "Address" DROP CONSTRAINT "Address_user_id_fkey";
+
+-- AlterTable
+ALTER TABLE "Review" ALTER COLUMN "rating" SET DATA TYPE DECIMAL(2,1);
+
+-- CreateTable
+CREATE TABLE "Recomendation" (
+    "id" TEXT NOT NULL,
+    "user_id" INTEGER NOT NULL,
+    "book_id" INTEGER NOT NULL,
+
+    CONSTRAINT "Recomendation_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "Recomendation" ADD CONSTRAINT "Recomendation_book_id_fkey" FOREIGN KEY ("book_id") REFERENCES "Book"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Recomendation" ADD CONSTRAINT "Recomendation_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
