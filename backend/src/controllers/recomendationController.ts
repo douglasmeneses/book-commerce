2b087e02ab6cd4a9de86bf47f8886ce7afd233b3
Marcela <marcelamartinstec@gmail.com>
2025-04-02 23:10:30 -0300

    fix(controller): ajuste no controller de recomendações

--- backend/src/controllers/recomendationController.ts
+++ backend/src/controllers/recomendationController.ts
@@ -1,20 +1,62 @@
 import { Request, Response } from "express";
+import { PrismaClient } from "@prisma/client";
 import recomendationService from "../services/recomendationService";
 
-const recomendationController = {
-  async getBookRecommendations(req: Request, res: Response) {
-    const user_id = parseInt(req.params.user_id);
+const prisma = new PrismaClient();
 
+const RecommendationController = {
+
+  async getRecommendations(req: Request, res: Response) {
     try {
-      const books = await recomendationService.getBookRecommendations(user_id);
+      const { uuid } = req.params;
 
-      return res.status(200).json(books);
+      const user = await prisma.user.findUnique({
+        where: { uuid },
+        select: { id: true },
+      });
+
+      if (!user) {
+        return res.status(404).json({ error: "Usuário não encontrado" });
+      }
+
+      const books = await recomendationService.getBookRecommendations(user.id);
+
+      if ("error" in books) {
+        return res.status(400).json(books);
+      }
+
+      return res.json(books);
     } catch (error) {
-      return res
-        .status(500)
-        .json({ error: "Error getting book recommendations" });
+      console.error("Erro ao buscar recomendações:", error);
+      return res.status(500).json({ error: "Erro interno do servidor" });
     }
   },
+
+  async registerRecommendation(req: Request, res: Response) {
+    try {
+      const { uuid, book_id } = req.body;
+
+      if (!uuid || !book_id) {
+        return res.status(400).json({ error: "UUID do usuário e ID do livro são obrigatórios." });
+      }
+
+      const user = await prisma.user.findUnique({
+        where: { uuid },
+        select: { id: true },
+      });
+
+      if (!user) {
+        return res.status(404).json({ error: "Usuário não encontrado" });
+      }
+
+      const recommendation = await recomendationService.registerBook(book_id, user.id);
+
+      return res.status(201).json(recommendation);
+    } catch (error) {
+      console.error("Erro ao armazenar recomendação:", error);
+      return res.status(500).json({ error: "Erro interno do servidor" });
+    }
+  }
 };
 
-export default recomendationController;
+export default RecommendationController;
