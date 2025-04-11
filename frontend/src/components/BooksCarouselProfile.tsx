2aa10b34c66472f50755ce5f239dd53421468fc6
Gabriel Macena <gabriel.macena10@hotmail.com>
2025-04-11 19:29:13 -0300

    BOOK-105

--- frontend/src/components/BooksCarouselProfile.tsx
+++ frontend/src/components/BooksCarouselProfile.tsx
@@ -25,13 +25,15 @@ export default function BooksPerfilCarousel({ books }: BookCarouselProps) {
     return () => clearTimeout(timer);
   }, []);
 
+  const isBooksArray = Array.isArray(books);
+
   return (
     <div className="w-full flex flex-col items-center">
       {Loading ? (
         <div className="w-full flex justify-center items-center h-40">
           <LoaderCircle className="animate-spin text-[#e67e22]" size={40} />
         </div>
-      ) : books.length > 0 ? (
+      ) : isBooksArray && books.length > 0 ? (
         <Carousel
           className="w-full max-w-4xl mt-5"
           opts={{
@@ -68,7 +70,7 @@ export default function BooksPerfilCarousel({ books }: BookCarouselProps) {
                       <p className="text-xs text-gray-600 line-clamp-1">
                         {book.authors.map((author, index) => (
                           <span key={index}>
-                            {author.author.name}{" "}
+                            {author.author.name}
                             {index < book.authors.length - 1 ? ", " : ""}
                           </span>
                         ))}
@@ -88,7 +90,7 @@ export default function BooksPerfilCarousel({ books }: BookCarouselProps) {
               </CarouselItem>
             ))}
           </CarouselContent>
-          <CarouselPrevious className=" absolute">{"<"}</CarouselPrevious>
+          <CarouselPrevious className="absolute">{"<"}</CarouselPrevious>
           <CarouselNext className="">{">"}</CarouselNext>
         </Carousel>
       ) : (
