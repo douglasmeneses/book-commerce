9073c21fc60521866b90c12f54291286e6aed9d0
Marcela Martins <marcelamartinstec@gmail.com>
2025-05-07 18:05:18 -0300

    Delete backend/prisma/migrations/20250505230930_teste_docker/migration.sql

--- backend/prisma/migrations/20250505230930_teste_docker/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to alter the column `rating` on the `Book` table. The data in that column could be lost. The data in that column will be cast from `Decimal(2,1)` to `DoublePrecision`.
-
-*/
--- AlterTable
-ALTER TABLE "Book" ALTER COLUMN "rating" SET DATA TYPE DOUBLE PRECISION;
